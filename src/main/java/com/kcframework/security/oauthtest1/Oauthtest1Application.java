5f9a2a16f8f54a61a615e394f56c971d28606e92
Jayang <Jayang@DESKTOP-FTGA4BL>
2018-10-17 12:55:19 +0800

    服务器库速度太慢  改为本地库链接

--- src/main/java/com/kcframework/security/oauthtest1/Oauthtest1Application.java
+++ src/main/java/com/kcframework/security/oauthtest1/Oauthtest1Application.java
@@ -2,9 +2,12 @@ package com.kcframework.security.oauthtest1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class Oauthtest1Application {
 
 	public static void main(String[] args) {
